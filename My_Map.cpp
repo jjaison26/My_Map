c5ef9d5a5f8e1a5e3e12db18a9479ced04b55c02
jjaison26 <jj89177@mail.harpercollege.edu>
2024-11-07 16:09:30 -0600

    fixed my_map
    fixed my_map

--- My_Map.cpp
+++ My_Map.cpp
@@ -6,11 +6,11 @@ int mult(int x) {
     return x * 5;
 }
 
-vector<int> my_map(vector<int> my_vec , int (my_fun(int)) ) {
+vector<int> my_map(vector<int> my_vec , int ((*my_fun)(int)) ) {
     vector<int> my_vector;
     
     for(int i = 0; i < my_vec.size(); i++) {
-        my_vector.push_back(*my_fun(my_vec[i]));
+        my_vector.push_back((*my_fun)(my_vec[i]));
     }
     return my_vector;
     
@@ -19,6 +19,8 @@ vector<int> my_map(vector<int> my_vec , int (my_fun(int)) ) {
 int main() {
     vector<int> my_v = {1, 2, 3};
     vector<int> result = my_map(my_v, &mult);
-    
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
+    }
     return 0;
 }
